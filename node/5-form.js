79ec098fdb075b3ccf4adb9b25b5727e18263a48
Mi Yon Kim <miyon@augusta.cs.pdx.edu>
2019-07-19 18:38:02 -0700

    1.Request post method from /form. 2.Get POST data and parse. Then add to the global value. 3.list global object.(iterate and write).

--- node/5-form.js
+++ node/5-form.js
@@ -34,3 +34,74 @@ var querystring = require('querystring'); // do not change this line
 // [the server restarts and looses all messages]
 
 // http://localhost:8080/list should return '' in plain text
+
+var g_myObj = {};	//set global variable.
+
+var server = http.createServer(function( req, res) {  
+    if (req.url === '/form') {
+        res.writeHead(200, {'Content-Type': 'text/html'});  
+        res.write('<!DOCTYPE html>');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<form action="/new" method="post">');
+        res.write('<input type="text" name="name">');
+        res.write('<input type="text" name="message">');
+        res.write('<input type="submit" value="submit">');
+        res.write('</form>');
+        res.write('</body>');
+        res.write('</html>');
+        
+        res.end();
+    }
+	else if (req.url === '/new') {
+		//request post method from /form.
+        if (req.method == 'POST') {
+			var body = '';
+
+			req.on('data', function (data) {
+				body += data;
+
+				// Too much POST data, kill the connection!
+				// 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+				if (body.length > 1e6)
+					req.connection.destroy();
+			});
+			
+			//parse body with querystring and set to g_myObj(global variable).
+			req.on('end', function () {
+                var post = querystring.parse(body);
+                g_myObj[post.name] = post.message;
+			});
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.write('thank you for your message');
+            res.end();
+		}
+    }
+	else if (req.url === '/list') {
+		//list values of name and message from g_myObj
+        res.writeHead(200, {'Content-Type': 'text/plain'});  
+		var size = Object.keys(g_myObj).length;
+		if (size == 0)
+			res.write('');
+		else if (size == 1){
+			for (var key in g_myObj){
+                res.write(`${key}: ${g_myObj[key]}`);
+			}
+		}
+        else {
+            console.log(g_myObj);
+			var i= 0;
+			for (var key in g_myObj){
+                res.write(`${key}: ${g_myObj[key]}`);
+				if (i != Object.keys(g_myObj).length-1)
+					res.write('\n');
+				i++;   
+			}
+        }
+        res.end();
+    }
+	
+    else
+        res.end();
+});
+server.listen(process.env.PORT || 8080);
