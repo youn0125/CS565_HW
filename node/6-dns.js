16a32838e7de5e320237ecb238134fd967d95cc9
Mi Yon Kim <miyon@augusta.cs.pdx.edu>
2019-07-22 00:59:35 -0700

    1. Get ip addresses using dns.resolve4 function. 2. Iterate an addresses array and return.

--- node/6-dns.js
+++ node/6-dns.js
@@ -6,5 +6,20 @@ var dns = require('dns'); // do not change this line
 // http://localhost:8080/pdx.edu should return '131.252.115.150' in plain text (address might be different, there could even be multiple addresses)
 
 // http://localhost:8080/sniklaus.com should return '216.239.36.21\n216.239.38.21\n216.239.32.21\n216.239.34.21' in plain text (addresses / order might be different)
+var server = http.createServer(function( req, res) {  
+	res.writeHead(200, {'Content-Type': 'text/plain'});
 
-// http://localhost:8080/error should return 'error' in plain text
+	dns.resolve4(req.url.substr(1), function (err, addresses) {
+        if(err)
+            res.write('error');
+		else{
+			for (const addr of addresses){
+				res.write(addr);
+				if(addr != addresses[addresses.length-1])
+					res.write('\n');
+			}
+		}
+        res.end();
+    });
+});
+server.listen(process.env.PORT || 8080);
