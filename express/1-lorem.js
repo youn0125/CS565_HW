4e3c55d02635fa9d423067d6077b7bfdf2aba2a4
Mi Yon Kim <miyon@augusta.cs.pdx.edu>
2019-07-31 00:16:19 -0700

    1. Used express framework. 2. Used GET method route to respond to client requests.

--- express/1-lorem.js
+++ express/1-lorem.js
@@ -3,3 +3,13 @@
 var express = require('express'); // do not change this line
 
 // http://localhost:8080/lorem should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
+const server = express();
+
+//GET method route.
+server.get('/lorem', function(req,res) {
+    res.status(200);
+    res.set({'Content-type': 'text/html'});
+    res.send('<!DOCTYPE html><html><body>lorem ipsum</body></thml>');
+});
+
+server.listen(process.env.PORT || 8080);
