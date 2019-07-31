fcb3edbc00e1387484e767af492b49d3bd42e31d
Mi Yon Kim <miyon@augusta.cs.pdx.edu>
2019-07-31 01:16:00 -0700

    1. Use bodyparser middleware to parse body data. 2. Save to global object from body data. 3. Display global object.

--- express/5-form.js
+++ express/5-form.js
@@ -38,3 +38,53 @@ var parser = require('body-parser'); // do not change this line
 // [the server restarts and looses all messages]
 
 // http://localhost:8080/list should return '' in plain text
+
+const server = express();
+var g_myObj = {};	//set global variable.
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = parser.urlencoded({ extended: false })
+
+server.get('/form', function(req,res) {
+    res.status(200);
+    res.set({'Content-type': 'text/html'});
+    res.write('<!DOCTYPE html>');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<form action="/new" method="post">');
+    res.write('<input type="text" name="name">');
+    res.write('<input type="text" name="message">');
+    res.write('<input type="submit" value="submit">');
+    res.write('</form>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+});
+server.post('/new', urlencodedParser, function(req,res) {
+    g_myObj[req.body.name] = req.body.message;
+    res.set({'Content-type': 'text/plain'});
+	res.send('thank you for your message');
+});
+server.get('/list', function(req,res) {
+    res.status(200);
+    res.set({'Content-type': 'text/plain'});
+	var size = Object.keys(g_myObj).length;
+	if (size == 0)
+		res.write('');
+	else if (size == 1){
+		for (var key in g_myObj){
+            res.write(`${key}: ${g_myObj[key]}`);
+		}
+	}
+    else {
+		var i= 0;
+		for (var key in g_myObj){
+            res.write(`${key}: ${g_myObj[key]}`);
+			if (i != Object.keys(g_myObj).length-1)
+				res.write('\n');
+			i++;   
+		}
+    }
+    res.end();
+});
+
+server.listen(process.env.PORT || 8080);
