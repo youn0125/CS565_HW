3e3657885436716c2c6b66598e74a2dc8eab082e
Mi Yon Kim <miyon@augusta.cs.pdx.edu>
2019-07-31 00:10:36 -0700

    1. Use express-session middleware to store cookies in the server side. 2. set page_view property to session object. 3. Parse cookies and display the history.

--- express/4-sessions.js
+++ express/4-sessions.js
@@ -2,6 +2,7 @@
 
 var express = require('express'); // do not change this line
 var session = require('express-session'); // do not change this line
+const cookieParser = require('cookie-parser');
 
 // preface: use the express-session middleware with the memorystore session storage which should make this task rather easy
 
@@ -26,3 +27,38 @@ var session = require('express-session'); // do not change this line
 // [the server restarts and looses all cookies]
 
 // http://localhost:8080/servus should return 'you must be new' in plain text and implicitly set an ident cookie by using the session middleware
+
+const server = express();
+server.use(cookieParser());
+server.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+  }));
+
+server.get('/:parameter', function(req,res,next) {
+	//set page_views property to session object.
+    if(req.session.page_views){
+        req.session.page_views++;
+        res.status(200);
+        res.set({'Content-type': 'text/plain'});
+        var history = '';
+        for (var key in req.cookies) {
+			//exclude the connect.sid cookie.
+            if(key !== 'connect.sid')
+                history += '\n  /' + key;
+          }
+        res.set('Set-cookie', `${[req.params.parameter]} = 1`);
+        res.send('your history:' + history);
+     } else {
+        req.session.page_views = 1;
+        res.status(200);
+        res.set({'Content-type': 'text/plain'});
+		//set cookie with parameter.
+        res.set('Set-cookie', `${[req.params.parameter]} = 1`);
+        res.send("you must be new");
+     }
+
+});
+
+server.listen(process.env.PORT || 8080);
