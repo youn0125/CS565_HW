4fbe700a90e723b76e00e51774c56ef13e75e30b
Mi Yon Kim <miyon@augusta.cs.pdx.edu>
2019-08-01 01:42:03 -0700

    1. Use the passport middleware and set the grant option. 2. Apply the authentication to specific routes.

--- express/6-auth.js
+++ express/6-auth.js
@@ -13,3 +13,51 @@ var strategy = require('passport-http'); // do not change this line
 // http://localhost:8080/world should return 'only accessible when logged in' in plain text if the user is authenticated, otherwise it should prompt for the username and password
 
 // http://localhost:8080/test should return 'only accessible when logged in' in plain text if the user is authenticated, otherwise it should prompt for the username and password
+const server = express();
+var authData = {
+  userid: 'test',
+  password: 'logmein',
+};
+passport.use(new strategy.BasicStrategy(
+  function(userid, password, done) {
+    if (userid === authData.userid) {
+      if (password === authData.password) {
+          return done(null, authData, {
+              message: 'only accessible when logged in'
+          });
+      } else {
+          return done(null, false, {
+              message: 'Incorrect password.'
+          });
+      }
+  } else {
+      return done(null, false, {
+          message: 'Incorrect username.'
+      });
+  }
+  }
+));
+
+server.get('/hello', function(req,res) {
+  res.status(200);
+  res.set({'Content-type': 'text/plain'});
+  res.send('accessible to everyone');
+});
+
+server.get('/world', 
+  passport.authenticate('basic', { session: false }),
+  function(req, res) {
+    res.json(req.user);
+  }
+);
+
+server.get('/test', 
+  passport.authenticate('basic', { session: false }),
+  function(req, res) {
+    res.status(200);
+    res.set({'Content-type': 'text/plain'});
+    res.send('only accessible when logged in');
+  }
+);
+
+server.listen(process.env.PORT || 8080);
